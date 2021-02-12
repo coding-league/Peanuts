4a4304085e42c99784223453417658458b8553c8
Hadise87 <h.shirudi@yahoo.com>
2021-02-12 23:16:33 +0900

    onmouseover of gang pictures

---
+++ About/about.js
@@ -0,0 +1,93 @@
+function changecharlie1() {
+  document.getElementById("myimg1").src = "../About/about-img/charlie-c.jpg";
+     }
+    function changecharlie2(){
+     document.getElementById("myimg1").src = "../About/about-img/charliebrown.jpg";
+     }
+
+
+function snoopy1() {
+ document.getElementById("myimg2").src = "../About/about-img/snoopy-c.jpg";
+ }
+function snoopy2(){
+document.getElementById("myimg2").src = "../About/about-img/snoopy-bw.jpg";
+}
+
+
+
+function wood1() {
+document.getElementById("myimg3").src = "../About/about-img/wo-color.jpg";
+}
+function wood2(){
+ document.getElementById("myimg3").src = "../About/about-img/wo-bw.jpg";
+ }
+
+
+
+
+function lucy1() {
+document.getElementById("myimg4").src = "../About/about-img/lu-color.jpg";
+ }
+function lucy2(){
+document.getElementById("myimg4").src = "../About/about-img/lu-bw.jpg";
+ }
+
+
+
+
+function linus1() {
+document.getElementById("myimg5").src = "../About/about-img/li-color.jpg";
+}
+function linus2(){
+ document.getElementById("myimg5").src = "../About/about-img/li-bw.jpg";
+ }
+
+
+ function sally1() {
+  document.getElementById("myimg6").src = "../About/about-img/sa-color.jpg";
+  }
+  function sally2(){
+  document.getElementById("myimg6").src = "../About/about-img/sa-BW.jpg";
+ }
+
+
+function schroder1() {
+  document.getElementById("myimg7").src = "../About/about-img/schroder-co.jpg";
+    }
+    function schroder2(){
+  document.getElementById("myimg7").src = "../About/about-img/schroder-BW.jpg";
+  }
+
+
+
+function pigpen1() {
+  document.getElementById("myimg8").src = "../About/about-img/pigpen-color.jpg";
+    }
+    function pigpen2(){
+  document.getElementById("myimg8").src = "../About/about-img/pigpen-BW.jpg";
+  }
+
+
+  function franklin1() {
+    document.getElementById("myimg9").src = "../About/about-img/franklin-co.jpg";
+      }
+      function franklin2(){
+    document.getElementById("myimg9").src = "../About/about-img/franklin-BW.jpg";
+    }
+
+    function peppermint1() {
+      document.getElementById("myimg10").src = "../About/about-img/peppermint-color.jpg";
+        }
+        function peppermint2(){
+      document.getElementById("myimg10").src = "../About/about-img/peppermint-BW.jpg";
+      }
+
+      
+      function marcie1() {
+        document.getElementById("myimg11").src = "../About/about-img/marcie-color.jpg";
+          }
+          function marcie2(){
+        document.getElementById("myimg11").src = "../About/about-img/marcie-BW.jpg";
+        }
+    
+
