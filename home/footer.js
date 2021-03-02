667f15beb91f149f7012e303d19169f9a5a5bfbb
Hadise87 <h.shirudi@yahoo.com>
2021-03-02 18:45:20 +0900

    make footer.js and footer.css and add it to all files

--- home/footer.js
+++ home/footer.js
@@ -1,10 +1,8 @@
 document.querySelector('body').insertAdjacentHTML('Afterend', `
 <footer>
-  <section class="pnut-footer">
-    <div class="row">
-      <div class="footer-menu">
-        <ul class="footer-nav">
-          <li> <a href="#">About</a></li>
+  <div class="footer-nav">
+    <ul class="footer-nav">
+      <li> <a href="#">About</a></li>
           <li> <a href="#">Take care</a></li>
           <li> <a href="#">Watch</a></li>
           <li> <a href="#">Activities</a></li>
@@ -12,17 +10,13 @@ document.querySelector('body').insertAdjacentHTML('Afterend', `
           <li> <a href="#">FAQs</a></li>
           <li> <a href="#">Terms of Use</a></li>
           <li> <a href="#">Privacy and Cookies Plicies</a></li>
-        </ul>
-      </div>
-    </div>
-    <div class="row">
-        <div class="col span-1-of-2">
-          <img src="../home/images/pnt-logo.jpg" alt="#">
-        </div>
-        <div class="col span-2-of-2">
-          <p> &copy;Peanuts Worldwide LLC </p>
-        </div>
-    </div>
-</section>
+    </ul>
+  </div>
 </footer>
+  <div class="legal">
+    <a href="#"><img src="../home/pea-llc.png" alt="#" height="25px" width="75px"></a>
+    <p> &copy;Peanuts Worldwide LLC </p>
+  </div>
+
+
 `);
